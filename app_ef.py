fdcd6f6d4c440c4fcea5915900db13c70c49162d
pframoss <pf.ramoss@alum.up.edu.pe>
2025-11-24 11:38:27 -0500

    Update app_ef.py

--- app_ef.py
+++ app_ef.py
@@ -155,7 +155,7 @@ Responde de forma concisa y clara en español.
 # =======================
 
 st.set_page_config(page_title="Chat PDF con MongoDB + Gemini + Cohere: ", page_icon="💬")
-st.title("💬 Chat PDF con MongoDB + Gemini + Cohere: "+USER)
+st.title("💬 Chat PDF con MongoDB + Gemini + Cohere FINAL: "+USER)
 
 archivo_pdf = st.file_uploader("📤 Sube un PDF", type=["pdf"])
 
@@ -191,3 +191,4 @@ for msg in st.session_state.historial:
         st.chat_message("user").write(msg["texto"])
     else:
         st.chat_message("assistant").write(msg["texto"])
+
